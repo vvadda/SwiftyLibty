e872dd1cecf0f8e7e730052ad62223f40923460e
www <egorenkovvadim@gmail.com>
2019-05-08 01:01:49 +0300

    Initial project setup

---
+++ SwiftyLibty/SwiftyLibty.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibty.h
+//  SwiftyLibty
+//
+//  Created by Vadim Yehorenkov on 5/8/19.
+//  Copyright © 2019 Vadim Yehorenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLibty.
+FOUNDATION_EXPORT double SwiftyLibtyVersionNumber;
+
+//! Project version string for SwiftyLibty.
+FOUNDATION_EXPORT const unsigned char SwiftyLibtyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibty/PublicHeader.h>
+
+
---
+++ SwiftyLibtyExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibtyExamples
+//
+//  Created by Vadim Yehorenkov on 5/8/19.
+//  Copyright © 2019 Vadim Yehorenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
