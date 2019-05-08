6f54e32792988df89389b94fa5f09a820140b7f0
www <egorenkovvadim@gmail.com>
2019-05-08 11:46:41 +0300

    Added test cases

---
+++ SwiftyLibty/SwiftyLibty.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLibty.swift
+//  SwiftyLibty
+//
+//  Created by Vadim Yehorenkov on 5/8/19.
+//  Copyright © 2019 Vadim Yehorenkov. All rights reserved.
+//
+
+
+public final class SwiftyLibty {
+    
+    let name = "SwiftyLibty"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
+
--- SwiftyLibtyTests/SwiftyLibtyTests.swift
+++ SwiftyLibtyTests/SwiftyLibtyTests.swift
@@ -11,24 +11,19 @@ import XCTest
 
 class SwiftyLibtyTests: XCTestCase {
 
+    var swiftyLibty: SwiftyLibty!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLibty = SwiftyLibty()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(swiftyLibty.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLibty.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
+
